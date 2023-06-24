662efdfd9e5aae36d23e8d56481c1fafb71ca6db
Andreas Nilsson <11990635+andreasnilssondev@users.noreply.github.com>
2023-06-24 06:34:26 +0200

    make timer work properly

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css';
- 
-const startButton =
-  document.querySelector<HTMLButtonElement>('[data-js="start"]')!;
-const stopButton =
-  document.querySelector<HTMLButtonElement>('[data-js="stop"]')!;
+
+const startPauseButton = document.querySelector<HTMLButtonElement>(
+  '[data-js="start-pause"]'
+)!;
+const resetButton =
+  document.querySelector<HTMLButtonElement>('[data-js="reset"]')!;
 const timerMinutesElement = document.querySelector<HTMLSpanElement>(
   '[data-js="timer-minutes"]'
 )!;
@@ -14,31 +15,70 @@ const timerMilliSecondsElement = document.querySelector<HTMLSpanElement>(
   '[data-js="timer-milliseconds"]'
 )!;
 
+let startTime: number | null = null;
+let pauseTime: number | null = null;
+let timePassed = 0;
 let isRunning = false;
 
-function updateTimer(timestamp: DOMHighResTimeStamp) {
-  if (isRunning) {
-    timerMinutesElement.innerHTML = Math.floor((timestamp / 1000 / 60) % 60)
-      .toString()
-      .padStart(2, '0');
-    timerSecondsElement.innerHTML = Math.floor((timestamp / 1000) % 60)
-      .toString()
-      .padStart(2, '0');
-    timerMilliSecondsElement.innerHTML = Math.floor(timestamp % 1000)
-      .toString()
-      .padStart(3, '0');
-    requestAnimationFrame(updateTimer);
+function render() {
+  timerMinutesElement.innerHTML = Math.floor((timePassed / 1000 / 60) % 60)
+    .toString()
+    .padStart(2, '0');
+  timerSecondsElement.innerHTML = Math.floor((timePassed / 1000) % 60)
+    .toString()
+    .padStart(2, '0');
+  timerMilliSecondsElement.innerHTML = Math.floor(timePassed % 1000)
+    .toString()
+    .padStart(3, '0');
+}
+
+function loop() {
+  if (isRunning === false) {
+    return;
   }
+
+  // call requestAnimationFrame before running the update logic so the browser can plan ahead
+  requestAnimationFrame(loop);
+
+  const now = new Date().getTime();
+
+  if (startTime === null) {
+    // Handle the first update
+    startTime = now;
+    return;
+  }
+
+  if (pauseTime !== null) {
+    startTime = now - timePassed;
+    pauseTime = null;
+  }
+
+  timePassed = now - startTime;
+
+  render();
+}
+
+function pause() {
+  isRunning = false;
+  startPauseButton.innerHTML = 'Resume';
+  pauseTime = new Date().getTime();
+  startPauseButton.addEventListener('click', start, { once: true });
 }
 
 function start() {
   isRunning = true;
-  requestAnimationFrame(updateTimer);
+  startPauseButton.innerHTML = 'Pause';
+  startPauseButton.addEventListener('click', pause, { once: true });
+  requestAnimationFrame(loop);
 }
 
-function stop() {
+startPauseButton.addEventListener('click', start, { once: true });
+resetButton.addEventListener('click', () => {
+  // TODO: fix issues
   isRunning = false;
-}
-
-startButton.addEventListener('click', start);
-stopButton.addEventListener('click', stop);
+  startPauseButton.innerHTML = 'Start';
+  startTime = null;
+  pauseTime = null;
+  timePassed = 0;
+  render();
+});
